eed82151be55154772a34f92153deba8b9487d2d
yurii-spa <yuriycooleshov@gmail.com>
2026-05-20 18:54:26 +0200

    chore: add spa_core/agents/__init__.py

---
+++ spa_core/agents/__init__.py
@@ -0,0 +1,7 @@
+# SPA Agents
+from .monitoring_agent import MonitoringAgent
+from .data_agent import DataAgent
+from .strategy_agent import StrategyAgent
+from .ceo_agent import CEOAgent
+
+__all__ = ["MonitoringAgent", "DataAgent", "StrategyAgent", "CEOAgent"]
